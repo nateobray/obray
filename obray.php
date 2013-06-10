33238749d543d47876e983832948fc5a16bc51e6
nateobray <nathanobray@gmail.com>
2013-06-10 13:39:36 -0600

    Added Demo app and htaccess

--- obray.php
+++ obray.php
@@ -26,7 +26,9 @@
 	
 	*****************************************************************************/
 
-
+	
+	require_once("settings.php");
+	
 	// enable debugging
 	error_reporting(E_ALL);									   // Set error reporting to display all errors and types
 	ini_set('display_errors', true); 						   // PHP configuration set display errors to true
@@ -34,8 +36,7 @@
 	session_set_cookie_params(0);
 	session_start();												// Starts a session in PHP
 	
-	define('_SELF_', dirname(__FILE__).'/');                   // The should contain the path to your application
-	require_once '../core/ORouter.php';                        // include ORouter
+	require_once __PATH_TO_CORE__ . 'ORouter.php';                        // include ORouter
 	$router = new ORouter();                                   // instatiate ORouter
 	$router->route($_SERVER["REQUEST_URI"]);                   // call ORouter's "route" function
 			
