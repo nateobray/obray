de86a4801fa8326297873606817d05a57491f3ae
nateobray <nathanobray@gmail.com>
2013-08-11 00:02:01 +0000

    added ousers to the include list in orouter.php

--- core/ORouter.php
+++ core/ORouter.php
@@ -33,7 +33,8 @@
 	require_once 'functions.php';
 	require_once 'OObject.php';                                                           // the base object for all obray objects (basically everything will extend this or a class that has already extended it)
 	require_once 'ODBO.php';                                                              // object that extends OObject but includes database functionality and table definition support
-
+	require_once 'ousers.php';
+	
 	if (!class_exists( 'OObject' )) { die(); }
 
 	/********************************************************************************************************************
