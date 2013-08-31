c72760fc360751c14d5ec6fb8f7feb399ffcf999
nateobray <nathanobray@gmail.com>
2013-08-31 23:12:52 +0000

    simplified for documentation 2

--- core/ORouter.php
+++ core/ORouter.php
@@ -28,9 +28,6 @@
 
 
 	require_once 'settings.php';										                  // see if a setting file exists for a given application (looks at the base path where your obray.php file exists)
-
-	require_once 'dbug.php';                                                              // easy readout function (i.e. new dBug($yourvariable);
-	require_once 'functions.php';
 	require_once 'OObject.php';                                                           // the base object for all obray objects (basically everything will extend this or a class that has already extended it)
 	require_once 'ODBO.php';                                                              // object that extends OObject but includes database functionality and table definition support
 	
