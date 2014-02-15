660110a4f6a00f4ee19bc14b769d5fed7c4c9767
nateobray <nathanobray@gmail.com>
2014-02-15 16:10:37 -0700

    Set UTF-8 encoding

--- core/OObject.php
+++ core/OObject.php
@@ -38,7 +38,7 @@
 		
 		if( !isSet( $conn ) ){
 			try {
-		        $conn = new PDO('mysql:host='.__DBHost__.';dbname='.__DB__, __DBUserName__,__DBPassword__);
+		        $conn = new PDO('mysql:host='.__DBHost__.';dbname='.__DB__.';charset=utf8', __DBUserName__,__DBPassword__,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    } catch(PDOException $e) {
 		        echo 'ERROR: ' . $e->getMessage();
