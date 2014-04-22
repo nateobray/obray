d0af983ea525755ba9d42c85ee9db6fd18d35ecf
nateobray <nathanobray@gmail.com>
2014-04-21 19:07:30 -0600

    Update obray.php

--- obray.php
+++ obray.php
@@ -26,18 +26,16 @@
 	
 	*****************************************************************************/
 
-	
 	require_once("settings.php");
 	
-	// enable debugging
-	error_reporting(E_ALL);									   // Set error reporting to display all errors and types
-	ini_set('display_errors', true); 						   // PHP configuration set display errors to true
+	error_reporting(E_ALL);						// Set error reporting to display all errors and types
+	ini_set('display_errors', true); 				// PHP configuration set display errors to true
 	
 	session_set_cookie_params(0);
-	session_start();												// Starts a session in PHP
+	session_start();						// Starts a session in PHP
 	
-	require_once __PATH_TO_CORE__ . 'ORouter.php';                        // include ORouter
-	$router = new ORouter();                                   // instatiate ORouter
-	$router->route($_SERVER["REQUEST_URI"]);                   // call ORouter's "route" function
+	require_once __OBRAY_PATH_TO_CORE__ . 'ORouter.php';    	// include ORouter
+	$router = new ORouter();                                   	// instatiate ORouter
+	$router->route($_SERVER["REQUEST_URI"]);                   	// call ORouter's "route" function
 			
 ?>
