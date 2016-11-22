df28f98300ba4b071ffa6a3f8cb4682b5f552e11
Brock Taylor <brockt@maloufsleep.com>
2016-11-22 10:34:34 -0700

    updated OObject to iinclude stack trace

--- core/OObject.php
+++ core/OObject.php
@@ -617,6 +617,43 @@
 	    }
 		public function isError(){ return $this->is_error; }
 
+		public function getStackTrace($exception) {
+			$stackTrace = "";
+			$count = 0;
+			foreach ($exception->getTrace() as $frame) {
+				$args = "";
+				if (isset($frame['args'])) {
+					$args = array();
+					foreach ($frame['args'] as $arg) {
+						if (is_string($arg)) {
+							$args[] = "'" . $arg . "'";
+						} elseif (is_array($arg)) {
+							$args[] = "Array";
+						} elseif (is_null($arg)) {
+							$args[] = 'NULL';
+						} elseif (is_bool($arg)) {
+							$args[] = ($arg) ? "true" : "false";
+						} elseif (is_object($arg)) {
+							$args[] = get_class($arg);
+						} elseif (is_resource($arg)) {
+							$args[] = get_resource_type($arg);
+						} else {
+							$args[] = $arg;
+						}
+					}
+					$args = join(", ", $args);
+				}
+				$stackTrace .= sprintf( "#%s %s(%s): %s(%s)\n",
+					$count,
+					$frame['file'],
+					$frame['line'],
+					$frame['function'],
+					$args );
+				$count++;
+			}
+			return $stackTrace;
+		}
+
 		/***********************************************************************
 
 			GETTER AND SETTER FUNCTIONS
