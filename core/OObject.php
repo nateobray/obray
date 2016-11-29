aa280422b7a1639ae7417a5dd8c6621c8d639291
Brock Taylor <brockt@maloufsleep.com>
2016-11-29 14:07:33 -0700

    updated enum for OObject

--- core/OObject.php
+++ core/OObject.php
@@ -473,7 +473,7 @@
 					}
 				} catch (Exception $e) {
 					$this->throwError($e->getMessage());
-					$this->logError(oCoreProjectEnum::ODBO,$e);
+					$this->logError(oCoreProjectEnum::OOBJECT,$e);
 				}
 				return $this;
 			} else if (method_exists($this, "index")) {
@@ -484,7 +484,7 @@
 					}
 				} catch (Exception $e) {
 					$this->throwError($e->getMessage());
-					$this->logError(oCoreProjectEnum::ODBO,$e);
+					$this->logError(oCoreProjectEnum::OOBJECT,$e);
 				}
 				return $this;
 			} else {
