c8939b3a8bfd5e4a7da4f1e8b4df1af3c7a3a341
Nate Obray <nate@maloufsleep.com>
2017-04-21 14:59:24 -0600

    update to websocket

--- core/oWebSocket.php
+++ core/oWebSocket.php
@@ -434,7 +434,7 @@
 			}
 
 			// 3.	Prepare/send response
-			if( empty(Sec-WebSocket-Key) ){ $headers['Sec-WebSocket-Key']; return; }
+			if( empty($headers['Sec-WebSocket-Key']) ){ return; }
 			$secKey = $headers['Sec-WebSocket-Key'];
 			$secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
 			//hand shaking header
