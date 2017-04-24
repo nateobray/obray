f9bc5dda04b6aac403f6a92924096da437b52706
Nate Obray <nathanobray@gmail.com>
2017-04-24 14:08:22 -0600

    update websocket server

--- core/oWebSocketServer.php
+++ core/oWebSocketServer.php
@@ -255,7 +255,7 @@
 					if( !feof($changed_socket) ){
 
 						try{
-							$this->fread_stream($changed_socket,8*1024)
+							$this->fread_stream($changed_socket,8*1024);
 						} catch(Exception $err) {
 							$this->console("%s","Unable to read form socket: ".$err->getMessage()."\n","RedBold");
 							$this->disconnect($changed_socket);
