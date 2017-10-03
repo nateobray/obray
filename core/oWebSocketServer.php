fc024e728dbed00c301c72b8750a254b6068690b
Nate Obray <nate@maloufsleep.com>
2017-10-03 09:28:19 -0600

    update web socket server

--- core/oWebSocketServer.php
+++ core/oWebSocketServer.php
@@ -81,8 +81,7 @@
 							"local_cert"=>__WEB_SOCKET_CERT__, 
 							"local_pk"=>__WEB_SOCKET_KEY__, 
 							"passphrase" => __WEB_SOCKET_KEY_PASS__,
-							"ciphers" => 'HIGH',
-							"disable_compression" => true
+							"ciphers" => 'HIGH'
 						) 
 					) );
 				} catch( Exception $err ){
