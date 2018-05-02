4aa1d0de011b6b158dc8d6015fd42be7fe69375f
Cameron Bird <cameron@maloufsleep.com>
2018-05-02 15:30:34 -0600

    fixed exception when defaultValue not available

--- src/oInvoker.php
+++ src/oInvoker.php
@@ -11,14 +11,14 @@ Class oInvoker implements \obray\interfaces\oInvokerInterface
 
     /**
      * The invoke method attempts to call a specified method on an object
-     * 
+     *
      * @param mixed $object This is the object that contains the method we want to call
      * @param string $method The name of the function on the object you want to call
      * @param array $params This is an array of parameters to be passed to the method
-     * 
+     *
      * @return mixed
      */
-    
+
     public function invoke($object, $method, $params=[]){
 
         // reflect the object 
@@ -35,9 +35,14 @@ Class oInvoker implements \obray\interfaces\oInvokerInterface
         } catch (\ReflectionException $e) {
             throw new \obray\exceptions\ClassMethodNotFound("Unable to find object method.",404);
         }
-        
+
         // support legacy style methods
-        if( count($parameters) === 1 && is_array($parameters[0]->getDefaultValue()) && count($parameters[0]->getDefaultValue()) === 0 ){
+        if (
+            count($parameters) === 1 &&
+            $parameters[0]->isDefaultValueAvailable() &&
+            is_array($parameters[0]->getDefaultValue()) &&
+            count($parameters[0]->getDefaultValue()) === 0
+        ) {
             $object->$method($params);
             return $object;
         }
