bb6b7e22dd29016310a3aa940cb9021c43c44db2
Nate Obray <nathanobray@gmail.com>
2019-11-21 14:18:25 -0700

    remove debug output

--- core/OObject.php
+++ core/OObject.php
@@ -394,10 +394,8 @@
 
 		private function getContainerSingleton() {
 			if(self::$container == null){
-				$this->console("getContainerSignleton");
 				$builder = new \DI\ContainerBuilder();
 				$builder->addDefinitions(__OBRAY_SITE_ROOT__.'di-config.php');
-				$this->console("returning getContainerSignleton");
 				return self::$container = $builder->build();
 
 			}
@@ -469,9 +467,7 @@
                     	if (class_exists( "cRoot" ) || class_exists( "\cRoot" )) {
 							// do nothing
 						} else {
-							$this->console("Requiring: " . __OBRAY_SITE_ROOT__."controllers/cRoot.php\n");
 							require_once __OBRAY_SITE_ROOT__."controllers/cRoot.php";
-							$this->console("Requiring: " . __OBRAY_SITE_ROOT__."controllers/cRoot.php done\n");
 						}
                     }
                     if(empty($path)){
@@ -484,9 +480,7 @@
 					$doesNamespaceClassExist = $this->_namespacedClassExists($this->path, $obj_name);
 
 					if (!class_exists( $obj_name ) && !$doesNamespaceClassExist) {
-						$this->console("Requiring: " . $this->path . "\n");
 						require_once $this->path;
-						$this->console("Require: " . $this->path . " done\n");
 					}
 					$class_exists = false;
 
@@ -503,16 +497,12 @@
 						try{
 				    		//	CREATE OBJECT
 							if($isNamespacedPath){
-								$this->console("Creatinging container object\n");
 								$container = $this->getContainerSingleton();
-								$this->console("Calling make for: ".$obj_name."\n");
-								
 								$obj = $container->make($obj_name, [
 									'params' => $params,
 									'direct' => $direct,
 									'rPath' => $rPath
 								]);
-								$this->console("Done with make for: ".$obj_name."\n");
 							}
 							else {
 								$obj = new $obj_name($params,$direct,$rPath);
