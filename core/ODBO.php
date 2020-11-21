f62dba0a19a424158c9bdd40d9ee08f98b99ec1d
Nate Obray <nathanobray@gmail.com>
2020-11-21 13:05:07 +0000

    fix

--- core/ODBO.php
+++ core/ODBO.php
@@ -538,7 +538,7 @@
 	        $where_str = $this->getWhere($params,$values,$original_params);
 
 	        $this->sql = 'SELECT '.implode(',',$columns).' FROM '.$this->table . $this->getJoin() . $filter_join .$where_str . $order_by . $limit;
-			$statement = (!empty($this->reader) && $this->shouldUserReader)?$this->reader->prepare($this->sql):$this->dbh->prepare($this->sql);
+			$statement = (!empty($this->reader) && $this->shouldUseReader)?$this->reader->prepare($this->sql):$this->dbh->prepare($this->sql);
 			$this->shouldUserReader = true;
 	        forEach($values as $value){ if( is_integer($value) ){ $statement->bindValue($value['key'], trim($value['value']), PDO::PARAM_INT); } else { $statement->bindValue($value['key'], trim((string)$value['value']), PDO::PARAM_STR); } }
 			$statement->execute();
