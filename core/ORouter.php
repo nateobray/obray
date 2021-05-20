aede7d13f8530393970f6b9cf2425ffa086ed273
James Funk <piurafunk@gmail.com>
2021-05-20 13:44:07 -0600

    Search entire content-type header for application/json, if found at all, consider it a JSON request

--- core/ORouter.php
+++ core/ORouter.php
@@ -56,7 +56,7 @@
 
 			$php_input = file_get_contents("php://input");
 			if( !empty($php_input) && empty($params['data']) ){
-				if( $_SERVER["CONTENT_TYPE"] === 'application/json' ){
+				if( strpos($_SERVER["CONTENT_TYPE"], 'application/json' ) !== -1 ){
 					$params = (array)json_decode($php_input);
 				} else {
 					$params["data"] = $php_input;
